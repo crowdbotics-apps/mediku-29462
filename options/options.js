ac0762a3a95412e04cf9ac0ad4466b9d7a2c303c
Crowdbotics <team@crowdbotics.com>
2021-08-04 17:33:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mediku_29462Identifier",
+  url: "https://mediku_29462Identifier.botics.co",
+  api: "https://mediku_29462Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
