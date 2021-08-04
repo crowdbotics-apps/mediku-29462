d8940002577bc39984ea5ed4367d0aaa68238ae0
Crowdbotics <team@crowdbotics.com>
2021-08-04 17:46:45 +0000

    Update models

--- backend/mediku_29462/wsgi.py
+++ backend/mediku_29462/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediku_29462.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mediku_29462.settings")
 
 application = get_wsgi_application()
